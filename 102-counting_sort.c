e4bbc5d72a9d3dcb25edf9bded3f1600a49b3e5b
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-07-21 17:16:09 +0300

    Add counting sort

---
+++ 102-counting_sort.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * counting_sort - sorts an array of integers in ascending order using the
+ * Counting sort algorithm. Prints the counting array once it is set up.
+ * @array: array of integers to be sorted
+ * @size: size of array
+ *
+ */
+void counting_sort(int *array, size_t size)
+{
+	int *output, *count, i, max, len = size;
+
+	if (!array || size == 0)
+		return;
+	max = array[0];
+	for (i = 1; i < len; i++)
+		if (max < array[i])
+			max = array[i];
+	count = malloc(sizeof(int) * (max + 1));
+	if (!count)
+		exit(EXIT_FAILURE);
+	for (i = 0; i <= max; i++)
+		count[i] = 0;
+	for (i = 0; i < len; i++)
+		count[array[i]] += 1;
+	for (i = 1; i <= max; i++)
+		count[i] = count[i - 1] + count[i];
+	print_array(count, max + 1);
+
+	output = malloc(sizeof(int) * size);
+	if (!output)
+		exit(EXIT_FAILURE);
+	for (i = 0; i < len; i++)
+		output[i] = -1;
+	for (i = 0; i <= max; i++)
+	{
+		while (count[i])
+		{
+			if (output[count[i] - 1] == -1)
+				output[count[i] - 1] = i;
+			count[i] -= 1;
+		}
+	}
+	for (i = 0; i < len; i++)
+		array[i] = output[i];
+	free(output);
+	free(count);
+}
