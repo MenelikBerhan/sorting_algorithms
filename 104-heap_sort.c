42c45057b31653920feee22740dc935081f6a3de
Toby2507 <oluwatobisalau2000@gmail.com>
2023-07-23 13:34:51 +0100

    Add the function documentation for the heap_sort function

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,5 +1,11 @@
 #include "sort.h"
 
+/**
+ * heap_sort - sorts an array of integers in ascending order using the
+ * Heap sort algorithm
+ * @array: array to be sorted
+ * @size: size of array
+ */
 void heap_sort(int *array, size_t size)
 {
 }
