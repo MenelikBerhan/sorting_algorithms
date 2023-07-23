8a77e4eb87ecc1f132b30b73ead11cd8fed8d88e
Toby2507 <oluwatobisalau2000@gmail.com>
2023-07-23 13:25:07 +0100

    Add merge sort prototype to header file

--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define SORT_H
 
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -25,6 +26,7 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
 #endif
